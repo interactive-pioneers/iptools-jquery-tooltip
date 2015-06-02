7bf619446811795b7747aad6f05084f6856324b0
David Lehnen <dl@interactive-pioneers.de>
2015-06-02 15:41:48 +0200

    Refactor test.js

--- test/spec/test.js
+++ test/spec/test.js
@@ -8,34 +8,42 @@
       maxWidth: 200
     };
 
-    var tooltip = null;
+    var pluginName = 'plugin_iptTooltip';
+
+    var tooltip = $('#js_tooltip');
+
+    var object = null;
 
     describe('init', function() {
 
       beforeEach(function() {
-        tooltip = $('.js_tooltip').iptTooltip(config);
+        object = $('.js_tooltip').iptTooltip(config);
       });
 
       it('expected to construct object', function() {
-        return expect(tooltip).to.be.an.object;
+        return expect(object).to.be.an.object;
       });
 
       it('expected to set margin to ' + config.margin, function() {
-        return expect(tooltip.data('plugin_iptTooltip').settings.margin).to.equal(config.margin);
+        return expect(object.data(pluginName).settings.margin).to.equal(config.margin);
       });
 
       it('expected to set maxWidth to ' + config.maxWidth, function() {
-        return expect(tooltip.data('plugin_iptTooltip').settings.maxWidth).to.equal(config.maxWidth);
+        return expect(object.data(pluginName).settings.maxWidth).to.equal(config.maxWidth);
       });
 
       it('expected to have class tooltip--active', function() {
-        tooltip.trigger('mouseenter');
-        return expect($('#js_tooltip').hasClass('tooltip--active')).to.be.ok;
+        object.trigger('mouseenter');
+        return expect(tooltip.hasClass('tooltip--active')).to.be.ok;
       });
 
       it('expected to have class tooltip--top-left', function() {
-        tooltip.css({position: 'absolute', bottom: 0, right: 0}).trigger('mouseenter');
-        return expect($('#js_tooltip').hasClass('tooltip--top-left')).to.be.ok;
+        object.css({
+          position: 'absolute',
+          bottom: 0,
+          right: 0
+        }).trigger('mouseenter');
+        return expect(tooltip.hasClass('tooltip--top-left')).to.be.ok;
       });
 
     });
