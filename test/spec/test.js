253855b0b8d8a0b160898f7fa35b7e9298e9bd65
David Lehnen <dl@interactive-pioneers.de>
2015-06-02 13:33:21 +0200

    Add tests

--- test/spec/test.js
+++ test/spec/test.js
@@ -3,16 +3,29 @@
 (function() {
   describe('iptTooltip', function() {
 
+    var config = {
+      margin: 10,
+      maxWidth: 200
+    };
+
     var tooltip = null;
 
     describe('init', function() {
-      
+
       beforeEach(function() {
-        tooltip = $('.js_tooltip').iptTooltip();
+        tooltip = $('.js_tooltip').iptTooltip(config);
       });
-      
+
       it('expected to construct object', function() {
-        return expect(tooltip).to.exist;
+        return expect(tooltip).to.be.an.object;
+      });
+
+      it('expected to set margin to ' + config.margin, function() {
+        return expect(tooltip.data('plugin_iptTooltip').settings.margin).to.equal(config.margin);
+      });
+
+      it('expected to set maxWidth to ' + config.maxWidth, function() {
+        return expect(tooltip.data('plugin_iptTooltip').settings.maxWidth).to.equal(config.maxWidth);
       });
 
     });
