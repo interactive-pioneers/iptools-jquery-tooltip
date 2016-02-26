d5223487f97a7c47da3cc710d7be3dd0ac65e553
David Lehnen <dl@interactive-pioneers.de>
2016-02-26 16:03:10 +0100

    Add asynchroneous tests

--- test/spec/test.js
+++ test/spec/test.js
@@ -4,36 +4,36 @@
   describe('iptTooltip', function() {
 
     var config = {
+      delay: 0,
       margin: 10,
       maxWidth: 200
     };
 
     var pluginName = 'plugin_iptTooltip';
 
-    var tooltip = null;
-
-    var object = null;
+    var $trigger = null;
+    var $tooltip = null;
 
     describe('init', function() {
 
       beforeEach(function() {
-        object = $('.js_tooltip').iptTooltip(config);
+        $trigger = $('.js_tooltip').iptTooltip(config);
       });
 
       afterEach(function() {
-        // object.data(pluginName).destroy();
+        // $trigger.data(pluginName).destroy();
       });
 
       it('expected to construct object', function() {
-        return expect(object).to.be.an.object;
+        return expect($trigger.data(pluginName)).to.be.an.object;
       });
 
       it('expected to set margin to ' + config.margin, function() {
-        return expect(object.data(pluginName).settings.margin).to.equal(config.margin);
+        return expect($trigger.data(pluginName).settings.margin).to.equal(config.margin);
       });
 
       it('expected to set maxWidth to ' + config.maxWidth, function() {
-        return expect(object.data(pluginName).settings.maxWidth).to.equal(config.maxWidth);
+        return expect($trigger.data(pluginName).settings.maxWidth).to.equal(config.maxWidth);
       });
 
     });
@@ -41,28 +41,34 @@
     describe('open', function() {
 
       beforeEach(function() {
-        object = $('.js_tooltip').iptTooltip(config);
+        $trigger = $('.js_tooltip').iptTooltip(config);
       });
 
       afterEach(function() {
-        // object.data(pluginName).destroy();
-        tooltip = null;
+        // $trigger.data(pluginName).destroy();
+        $tooltip = null;
       });
 
-      it('expected to have class tooltip--active', function() {
-        object.trigger('mouseenter');
-        tooltip = object.data(pluginName).$tooltip;
-        return expect(tooltip.hasClass('tooltip--active')).to.be.ok;
+      it('expected to have class tooltip--active', function(done) {
+        $trigger.trigger('mouseenter');
+        $tooltip = $trigger.data(pluginName).$tooltip;
+        setTimeout(function() {
+          done();
+          return expect($tooltip.hasClass('tooltip--active')).to.be.ok;
+        }, config.delay + 5);
       });
 
-      it('expected to have class tooltip--top-left', function() {
-        object.css({
+      it('expected to have class tooltip--top-left', function(done) {
+        $trigger.css({
           position: 'absolute',
           bottom: 0,
           right: 0
         }).trigger('mouseenter');
-        tooltip = object.data(pluginName).$tooltip;
-        return expect(tooltip.hasClass('tooltip--top-left')).to.be.ok;
+        $tooltip = $trigger.data(pluginName).$tooltip;
+        setTimeout(function() {
+          done();
+          return expect($tooltip.hasClass('tooltip--top-left')).to.be.ok;
+        }, config.delay + 5);
       });
 
     });
@@ -70,20 +76,20 @@
     describe('close', function() {
 
       beforeEach(function() {
-        object = $('.js_tooltip').iptTooltip(config);
+        $trigger = $('.js_tooltip').iptTooltip(config);
       });
 
       afterEach(function() {
-        // object.data(pluginName).destroy();
-        tooltip = null;
+        // $trigger.data(pluginName).destroy();
+        $tooltip = null;
       });
 
       it('expected to not have class tooltip--active', function() {
-        object
+        $trigger
           .trigger('mouseenter')
           .trigger('mouseleave');
-        tooltip = object.data(pluginName).$tooltip;
-        return expect(tooltip.hasClass('tooltip--active')).to.be.not.ok;
+        $tooltip = $trigger.data(pluginName).$tooltip;
+        return expect($tooltip.hasClass('tooltip--active')).to.be.not.ok;
       });
 
     });
