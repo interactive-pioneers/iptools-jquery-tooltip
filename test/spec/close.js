c094b24f769c89c21cc7de321d89af683988822f
Ain Tohvri <at@interactive-pioneers.de>
2017-05-18 16:40:09 +0200

    Move close tests to dedicated specs file

--- test/spec/close.js
+++ test/spec/close.js
@@ -15,7 +15,7 @@
     var $tooltip = null;
     var selector = '.js_tooltip';
 
-    describe('init', function() {
+    describe('close', function() {
 
       beforeEach(function() {
         $tooltip = $(selector).iptTooltip(config);
@@ -25,16 +25,9 @@
         $tooltip.data(pluginName).destroy();
       });
 
-      it('expected to construct object', function() {
-        return expect($tooltip.data(pluginName)).to.be.an.object;
-      });
-
-      it('expected to set margin to ' + config.margin, function() {
-        return expect($tooltip.data(pluginName).settings.margin).to.equal(config.margin);
-      });
-
-      it('expected to set maxWidth to ' + config.maxWidth, function() {
-        return expect($tooltip.data(pluginName).settings.maxWidth).to.equal(config.maxWidth);
+      it('expected to not have class tooltip--active', function() {
+        $tooltip.trigger('mouseenter').trigger('mouseleave');
+        return expect($tooltip.hasClass('tooltip--active')).to.be.not.ok;
       });
 
     });
