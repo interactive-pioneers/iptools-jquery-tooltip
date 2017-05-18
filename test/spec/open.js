bbb8e61a4518ecffd7b779488011776d41424989
Ain Tohvri <at@interactive-pioneers.de>
2017-05-18 17:02:37 +0200

    DRY up open spec

--- test/spec/open.js
+++ test/spec/open.js
@@ -11,45 +11,38 @@
       margin: 10,
       maxWidth: 200
     };
-
     var pluginName = 'plugin_iptTooltip';
-
-    var $trigger = null;
     var $tooltip = null;
-    var tooltip = null;
     var selector = '.js_tooltip';
 
     describe('open', function() {
 
       beforeEach(function() {
-        $trigger = $(selector).iptTooltip(config);
-        tooltip = $('#js_tooltip');
+        $tooltip = $(selector).iptTooltip(config);
       });
 
       afterEach(function() {
-        $trigger.data(pluginName).destroy();
+        $tooltip.data(pluginName).destroy();
       });
 
       it('expected to have class tooltip--active', function(done) {
-        $trigger.trigger('mouseenter');
-        $tooltip = $trigger.data(pluginName).$tooltip;
         setTimeout(function() {
           done();
           return expect($tooltip.hasClass('tooltip--active')).to.be.ok;
         }, config.delay + 5);
+        $tooltip.trigger('mouseenter');
       });
 
       it('expected to have class tooltip--top-left', function(done) {
-        $trigger.css({
-          position: 'absolute',
-          bottom: 0,
-          right: 0
-        }).trigger('mouseenter');
-        $tooltip = $trigger.data(pluginName).$tooltip;
         setTimeout(function() {
           done();
           return expect($tooltip.hasClass('tooltip--top-left')).to.be.ok;
         }, config.delay + 5);
+        $tooltip.css({
+          position: 'absolute',
+          bottom: 0,
+          right: 0
+        }).trigger('mouseenter');
       });
 
     });
